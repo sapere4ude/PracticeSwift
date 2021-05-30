e6f1e5cee50975ba7ea1fd82bc89f551e276b07b
sapere4ude <eileenyoo2@gmail.com>
2021-05-30 22:14:49 +0900

    ch1. MyMemory

---
+++ MyMemory/MyMemory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Kant on 2021/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
