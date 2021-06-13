d2e1411648c1a59f134444234cfd8279fdff6bc1
sapere4ude <eileenyoo2@gmail.com>
2021-06-13 23:43:33 +0900

    Button & Label

--- Ch02_Button/Ch02_Button/ViewController.swift
+++ Ch02_Button/Ch02_Button/ViewController.swift
@@ -11,7 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1. 버튼 인스턴스 생성
+        let btn = UIButton(type: .system)
+        
+        // 2. 버튼 영역 정의
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
+        
+        btn.center = CGPoint(x: self.view.frame.size.width / 2, y: 100)
+        
+        // 3. 버튼 속성 정의
+        btn.setTitle("테스트 버튼", for: .normal)
+        
+        // 4. 버튼을 루트뷰에 추가
+        self.view.addSubview(btn)
+        
+        btn.addTarget(self, action: #selector(btnClcik(_:)), for: .touchUpInside)
+    }
+    
+    @objc func btnClcik(_ sender: Any) {
+        if let btn = sender as? UIButton {
+            btn.setTitle("클릭되었습니다.", for: .normal)
+        }
     }
 
 
---
+++ Ch02_InputForm/Ch02_InputForm/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  Ch02_InputForm
+//
+//  Created by Kant on 2021/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var paramEmail: UITextField!    // 상수가 아닌 변수를 사용하는 것도 체크해야함 -> 뷰 컨트롤러 내에서 제한없이 접근해야하기 때문에 맴버변수로 설정.
+    var paramUpdate: UISwitch!
+    var paramInterval: UIStepper!
+    var txtUpdate: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "설정"
+        
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        
+        lblEmail.font = UIFont.systemFont(ofSize: 14)
+        
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동갱신"
+        
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblUpdate)
+        
+        self.view.addSubview(lblEmail)
+        
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblInterval)
+        
+        self.paramEmail = UITextField() // 이곳에서 인스턴스로 만들어줌 (메모리 할당해주는 것)
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
+        self.paramEmail.font = UIFont.systemFont(ofSize: 13)
+        self.paramEmail.borderStyle = .roundedRect
+        self.paramEmail.adjustsFontSizeToFitWidth = true
+        self.paramEmail.placeholder = "이메일을 입력하세요"
+        
+        self.view.addSubview(self.paramEmail)
+        
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(self.paramUpdate)
+        
+        
+        self.paramInterval = UIStepper()
+        self.paramInterval.minimumValue = 0
+        self.paramInterval.maximumValue = 100
+        self.paramInterval.stepValue = 1        // 값 변경 단위
+        self.paramInterval.value = 0            // 초기값
+        
+        self.view.addSubview(self.paramInterval)
+        
+        self.txtUpdate = UILabel()
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.txtUpdate.font = UIFont.systemFont(ofSize: 14)
+        self.txtUpdate.textColor = UIColor.red
+        self.txtUpdate.text = "갱신함"
+        
+        self.view.addSubview(self.txtUpdate)
+        
+    }
+
+
+}
+
