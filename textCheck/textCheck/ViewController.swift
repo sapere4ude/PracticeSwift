ee5b072a1fa04e967b6befb6d1c4fa058ba2cde4
sapere4ude <eileenyoo2@gmail.com>
2021-06-16 23:36:16 +0900

    텍스트 검사 (swift 정규식 활용)

---
+++ textCheck/textCheck/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  textCheck
+//
+//  Created by Kant on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        passwordText.text = ""
+        passwordText.delegate = self
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if string == "\n" {
+            if isValidLengthPassword(pwd: textField.text!) == false {
+                print("@@@")
+            }
+            if isValidCharacterAndNumber(pwd: textField.text!) == false {
+                print("###")
+            }
+            if isValidatePasswordSuccessive(pwd: textField.text!) == false {
+                print("^ㅁ^")
+            }
+        }
+        return true
+    }
+}
+
+// 6자 미만 문자 입력시
+func isValidLengthPassword(pwd: String) -> Bool {
+    if pwd.count > 6 {
+        return true
+    } else {
+         return false
+    }
+}
+
+// 영문, 숫자 조합
+func isValidCharacterAndNumber(pwd: String) -> Bool {
+    let passwordRegEx = "^(?=.*[A-Za-z])(?=.*[0-9])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`']{6,10}$"
+    let predicate = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+    return predicate.evaluate(with: pwd)
+}
+
+// 동일한 문자/숫자를 3개 이상 사용
+func isValidatePasswordSuccessive(pwd: String) -> Bool {
+    
+    let passwordRegEx = "(.)\\1\\1" // 동일한 한 글자(문자, 숫자)가 3번 중복
+    let predicate = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+    return predicate.evaluate(with: pwd)
+}
