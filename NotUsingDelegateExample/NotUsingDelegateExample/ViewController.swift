124a3ac4bcc37e18d313bf53a480f485cc91eb16
sapere4ude <eileenyoo2@gmail.com>
2021-06-19 16:04:59 +0900

    Not Using DelegatePattern in UIView

---
+++ NotUsingDelegateExample/NotUsingDelegateExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotUsingDelegateExample
+//
+//  Created by Kant on 2021/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
