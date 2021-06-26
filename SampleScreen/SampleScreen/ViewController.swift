776491c764846951533a0fae271197c149000ab2
sapere4ude <eileenyoo2@gmail.com>
2021-06-26 22:55:39 +0900

    sample screen

---
+++ SampleScreen/SampleScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleScreen
+//
+//  Created by Kant on 2021/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
