fb90a8875c18d86bf5ce0a9a6693c18382ffef84
sapere4ude <eileenyoo2@gmail.com>
2021-12-20 22:45:59 +0900

    - 스크롤뷰, 스택뷰 연습

--- SampleScreen/SampleScreen/ViewController.swift
+++ SampleScreen/SampleScreen/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let transparentView = UIView()
+    let selectChannelTableView = UITableView()
+    
+    var dataSource = [String]()
+    
+    @IBOutlet weak var selectedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    @IBAction func selectChannel(_ sender: Any) {
+        let window = UIApplication.shared.keyWindow
+        transparentView.frame = window?.frame ?? self.view.frame
+        self.view.addSubview(transparentView)
+        
+        selectChannelTableView.frame = CGRect(x: selectChannelTableView.frame.origin.x, y: selectChannelTableView.frame.origin.y, width: selectChannelTableView.frame.width, height: 0)
+        self.view.addSubview(selectChannelTableView)
+        selectChannelTableView.layer.cornerRadius = 5
+        
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        selectChannelTableView.reloadData()
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+        transparentView.addGestureRecognizer(tapgesture)
+        transparentView.alpha = 0
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0.5
+            self.selectChannelTableView.frame = CGRect(x: self.selectChannelTableView.frame.origin.x, y: self.selectChannelTableView.frame.origin.y + self.selectChannelTableView.frame.height + 5, width: self.selectChannelTableView.frame.width, height: CGFloat(self.dataSource.count * 50))
+        }, completion: nil)
+    }
+    
+    @objc func removeTransparentView() {
+             let frames = selectedButton.frame
+             UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+                 self.transparentView.alpha = 0
+                 self.selectChannelTableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+             }, completion: nil)
+         }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+             selectedButton.setTitle(dataSource[indexPath.row], for: .normal)
+             removeTransparentView()
+         }
+}
