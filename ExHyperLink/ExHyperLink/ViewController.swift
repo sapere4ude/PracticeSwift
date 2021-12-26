f7abc62e68b7e1c9ecfa3176d0492f613c154948
sapere4ude <eileenyoo2@gmail.com>
2021-12-26 21:44:11 +0900

    - 문자열 attribute 속성 추가

--- ExHyperLink/ExHyperLink/ViewController.swift
+++ ExHyperLink/ExHyperLink/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testLabel.text = "문자열에 특성 주는 것을 테스트합니당"
+
+        let generalAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.systemGreen,
+                                                                .underlineColor: NSUnderlineStyle.single.rawValue,
+                                                                .font: UIFont.boldSystemFont(ofSize: 20)]
+
+        // 문자열을 커스텀하기전에 객체를 만들어주는 것이 포인트
+        let mutableString = NSMutableAttributedString()
+    
+        mutableString.append(NSAttributedString(string: testLabel.text ?? "", attributes: generalAttributes))
+
+        testLabel.attributedText = mutableString
     }
-
-
 }
 
