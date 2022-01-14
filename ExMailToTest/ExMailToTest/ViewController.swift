519c91d78f9cbd6073ea65833593fe6b4b24890c
sapere4ude <eileenyoo2@gmail.com>
2022-01-14 23:51:23 +0900

    - 기본 메일 앱 연동 테스트

---
+++ ExMailToTest/ExMailToTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ExMailToTest
+//
+//  Created by Kant on 2022/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sendEmail(_ sender: Any) {
+        let email = "afreecakant@gmail.com"
+        if let url = URL(string: "mailto:\(email)") {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+}
+
