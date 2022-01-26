1fd64b807813eb006cc5612d63e93f3100ee9674
sapere4ude <eileenyoo2@gmail.com>
2022-01-26 22:03:59 +0900

    - Custom UISlider & System Volume Button connect

--- ExMPVolumeView/ExMPVolumeView/ViewController.swift
+++ ExMPVolumeView/ExMPVolumeView/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 import MediaPlayer
 import AVFoundation
+import StepSlider
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var slider: UISlider!
-
+    @IBOutlet weak var stepSlider: StepSlider!
+    
     var outputVolumeObserve: NSKeyValueObservation?
     let audioSession = AVAudioSession.sharedInstance()
+    
+    static func setVolume(_ volume: Float) {
+        let volumeView = MPVolumeView()
+        let slider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
+            slider?.value = volume
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +39,14 @@ class ViewController: UIViewController {
         
         outputVolumeObserve = audioSession.observe(\.outputVolume) { (audioSession, changes) in
             self.slider.value = audioSession.outputVolume
+//            self.stepSlider.index = UInt(round(audioSession.outputVolume * 10))
+
+            let outputVolumeUpdate = (audioSession.outputVolume * 10).rounded()
+            print("outputVolumeUpdate:\(outputVolumeUpdate)")
+            self.stepSlider.index = UInt(outputVolumeUpdate)
         }
+    }
+    @IBAction func soundVolumeChange(_ sender: Any) {
+        Self.setVolume(Float(self.stepSlider.index) * 0.1)
     }
 }
