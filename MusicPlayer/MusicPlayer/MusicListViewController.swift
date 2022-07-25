7cb37fddf78a68cb85f3dd436dd42883b09330bb
sapere4ude <eileenyoo2@gmail.com>
2022-07-26 08:48:45 +0900

    - 탭바컨트롤러 생성

---
+++ MusicPlayer/MusicPlayer/MusicListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MusicListViewController.swift
+//  MusicPlayer
+//
+//  Created by Kant on 2022/07/26.
+//
+
+import UIKit
+
+class MusicListViewController: UIViewController {
+
+    @IBOutlet weak var musicTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
