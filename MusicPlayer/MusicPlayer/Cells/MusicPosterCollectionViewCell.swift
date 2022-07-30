75814e8994edfbd5b2d91aa6a0752d6a28810de5
sapere4ude <eileenyoo2@gmail.com>
2022-07-30 22:00:14 +0900

    - 뮤직플레이어 개발 진행중

---
+++ MusicPlayer/MusicPlayer/Cells/MusicPosterCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MusicPosterCollectionViewCell.swift
+//  MusicPlayer
+//
+//  Created by Kant on 2022/07/26.
+//
+
+import UIKit
+
+class MusicPosterCollectionViewCell: UICollectionViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MusicPlayer/MusicPlayer/Models.swift
@@ -0,0 +1,22 @@
+//
+//  Models.swift
+//  MusicPlayer
+//
+//  Created by Kant on 2022/07/26.
+//
+
+import Foundation
+
+enum CellModel {
+    case collectionView(models: [CollectionTableCellModel], rows: Int)
+    case list(models: [ListCellModel])
+}
+
+struct ListCellModel {
+    let title: String
+}
+
+struct CollectionTableCellModel {
+    let title: String
+    let imageName: String
+}
