c6d44a6e6b7bf14db0215f16ba810829008c42a3
sapere4ude <eileenyoo2@gmail.com>
2022-08-19 23:26:08 +0900

    - MVVM 테스트 코드 진행

---
+++ ExMVVM/ExMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExMVVM
+//
+//  Created by Kant on 2022/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
