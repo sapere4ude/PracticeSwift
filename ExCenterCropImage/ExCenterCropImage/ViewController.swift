ac2c4f91feb9c806cf54dcc7da783da479ed3304
kant <eileenyoo2@gmail.com>
2022-10-07 23:29:45 +0900

    - 중앙정렬, 가로폭 확대 관련 테스트

--- ExCenterCropImage/ExCenterCropImage/ViewController.swift
+++ ExCenterCropImage/ExCenterCropImage/ViewController.swift
@@ -29,58 +29,50 @@ class ViewController: UIViewController {
 
     private let thumbnailImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.contentMode = .center
+        imageView.contentMode = .scaleToFill
         imageView.isUserInteractionEnabled = true
         return imageView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.addSubview(baseView)
-//        baseView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            baseView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-//            baseView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-//            baseView.widthAnchor.constraint(equalToConstant: 300),
-//            baseView.heightAnchor.constraint(equalToConstant: 150)
-//        ])
 
-        setBlurEffectUI()
-
+        view.addSubview(thumbnailImageView)
+        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+//            thumbnailImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            thumbnailImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -100),
+            thumbnailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 100),
+//            thumbnailImageView.widthAnchor.constraint(equalToConstant: 300),
+//            thumbnailImageView.heightAnchor.constraint(equalToConstant: 150)
+        ])
+        thumbnailImageView.image = UIImage(named: "sample")
+//            Bundle.main.url(forResource: "sample", withExtension: "png")
     }
     
-    func setBlurEffectUI() {
-//        baseView.addSubview(thumbnailImageView)
-        imageView.addSubview(thumbnailImageView)
-        
-        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            thumbnailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-//            thumbnailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-//            thumbnailImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            thumbnailImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            thumbnailImageView.widthAnchor.constraint(equalToConstant: 500),
-            thumbnailImageView.heightAnchor.constraint(equalToConstant: 400)
-            
-        ])
-        
-//        thumbnailImageView.addSubview(blurEffect)
-//        blurEffect.translatesAutoresizingMaskIntoConstraints = false
+//    func setBlurEffectUI() {
+////        baseView.addSubview(thumbnailImageView)
+////        imageView.addSubview(thumbnailImageView)
+//
+//        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
 //
 //        NSLayoutConstraint.activate([
-//            blurEffect.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-//            blurEffect.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-//            blurEffect.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-//            blurEffect.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+//            thumbnailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+////            thumbnailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+////            thumbnailImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+//            thumbnailImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+//            thumbnailImageView.widthAnchor.constraint(equalToConstant: 500),
+//            thumbnailImageView.heightAnchor.constraint(equalToConstant: 400)
+//
 //        ])
-
-        let url = URL(string: "https://iflv14.afreecatv.com/clip/20220915/131/a6c18144-7f44-4573-83ac-0f3a073c6778/244131_r.jpg")
-        let data = try! Data(contentsOf: url!)
-
-        thumbnailImageView.contentMode = .center
-        thumbnailImageView.image = UIImage(data: data)
-//        thumbnailImageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-    }
+//
+//        let url = URL(string: "https://iflv14.afreecatv.com/clip/20220915/131/a6c18144-7f44-4573-83ac-0f3a073c6778/244131_r.jpg")
+//        let data = try! Data(contentsOf: url!)
+//
+//        thumbnailImageView.contentMode = .center
+//        thumbnailImageView.image = UIImage(data: data)
+////        thumbnailImageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+//    }
 }
 
