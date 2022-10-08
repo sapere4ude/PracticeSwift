81183f6b2fd406cce47a0773873f9a5d88e3365a
kant <eileenyoo2@gmail.com>
2022-10-08 22:16:53 +0900

    - 이미지 뒤에 블러처리 자연스럽게 넣을 수 있는 방법

--- ExCenterCropImage/ExCenterCropImage/ViewController.swift
+++ ExCenterCropImage/ExCenterCropImage/ViewController.swift
@@ -9,17 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-    
-    var baseView: UIView = {
-        let view = UIView()
-        view.layer.cornerRadius = 6
-        view.clipsToBounds = true
-        view.isUserInteractionEnabled = true
-        return view
-    }()
-
-    // CATCH 타입일 경우 사용
     private var blurEffect: UIVisualEffectView = {
         let blurEffect = UIBlurEffect(style: .regular)
         let visualEffectView = UIVisualEffectView(effect: blurEffect)
@@ -34,45 +23,60 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    private let centerPositionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(thumbnailImageView)
-        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        setThumbnailImageView()
+        setBlurEffectUI()
+        setCenterPositionImage()
+    }
+    
+    func setBlurEffectUI() {
+        thumbnailImageView.addSubview(blurEffect)
+        
+        blurEffect.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
-//            thumbnailImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            blurEffect.leadingAnchor.constraint(equalTo: thumbnailImageView.leadingAnchor),
+            blurEffect.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor),
+            blurEffect.topAnchor.constraint(equalTo: thumbnailImageView.topAnchor),
+            blurEffect.bottomAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor)
+        ])
+    }
+    
+    func setThumbnailImageView() {
+        thumbnailImageView.image = UIImage(named: "sample")
+        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(thumbnailImageView)
+
+        NSLayoutConstraint.activate([
             thumbnailImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             thumbnailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -100),
             thumbnailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 100),
-//            thumbnailImageView.widthAnchor.constraint(equalToConstant: 300),
-//            thumbnailImageView.heightAnchor.constraint(equalToConstant: 150)
         ])
-        thumbnailImageView.image = UIImage(named: "sample")
-//            Bundle.main.url(forResource: "sample", withExtension: "png")
     }
     
-//    func setBlurEffectUI() {
-////        baseView.addSubview(thumbnailImageView)
-////        imageView.addSubview(thumbnailImageView)
-//
-//        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.activate([
-//            thumbnailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-////            thumbnailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-////            thumbnailImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-//            thumbnailImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-//            thumbnailImageView.widthAnchor.constraint(equalToConstant: 500),
-//            thumbnailImageView.heightAnchor.constraint(equalToConstant: 400)
-//
-//        ])
-//
-//        let url = URL(string: "https://iflv14.afreecatv.com/clip/20220915/131/a6c18144-7f44-4573-83ac-0f3a073c6778/244131_r.jpg")
-//        let data = try! Data(contentsOf: url!)
-//
-//        thumbnailImageView.contentMode = .center
-//        thumbnailImageView.image = UIImage(data: data)
-////        thumbnailImageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-//    }
+    func setCenterPositionImage() {
+        centerPositionImageView.image = UIImage(named: "sample")
+        centerPositionImageView.translatesAutoresizingMaskIntoConstraints = false
+        centerPositionImageView.layer.zPosition = 10
+        
+        view.addSubview(centerPositionImageView)
+        
+        NSLayoutConstraint.activate([
+            centerPositionImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            centerPositionImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            centerPositionImageView.widthAnchor.constraint(equalToConstant: 200),
+            centerPositionImageView.heightAnchor.constraint(equalToConstant: 200),
+        ])
+    }
 }
 
