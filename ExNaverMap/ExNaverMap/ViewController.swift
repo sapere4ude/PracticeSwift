efe8b3e8b32087a6458776900da999c2a918091d
kant <eileenyoo2@gmail.com>
2022-12-10 13:03:43 +0900

    - 네이버맵에 등록된 장소에만 마커를 찍을 수 있도록 작업

--- ExNaverMap/ExNaverMap/ViewController.swift
+++ ExNaverMap/ExNaverMap/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var pickMemos: [PickMemoModel]? = []
     
     var caption: String?
+    var isCreateMarker: Bool = true
+    var isRegisterCaption: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -88,7 +90,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
 // MARK: - 지도 위 내가 클릭한 곳으로 카메라 이동
 extension ViewController: NMFMapViewTouchDelegate {
+    
+    /*
+     didTap & point -> 마커가 있어야지만 타는 코드
+     caption 여부로 1차 판단
+     
+     */
+    
+    func mapView(_ mapView: NMFMapView, didTap symbol: NMFSymbol) -> Bool {
+        print("kant, test2")
+        if(symbol.caption.count > 0) {
+            caption = symbol.caption
+            pickMemos?.forEach({ pickMemo in
+                if pickMemo.caption == symbol.caption {
+                    isCreateMarker = false
+                }
+            })
+            isRegisterCaption = false
+            return false // 마커 만들 수 있다
+        } else {
+            return true // 마커 만들 수 없다
+        }
+    }
+    
+    // 캡션 값 없이 바로 진입 가능
     func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+        print("kant, test1")
+        // guard 문 -> 이 값이 맞아야해. 그렇지 않으면 조건문 타지 않아
+        guard isCreateMarker == true else { return }
+        guard isRegisterCaption == false else { return }
+        
         let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latlng.lat, lng: latlng.lng)) 
         cameraUpdate.animation = .easeIn
         mapView.moveCamera(cameraUpdate)
@@ -97,6 +128,9 @@ extension ViewController: NMFMapViewTouchDelegate {
         let pickMemo = PickMemoModel(latlng: latlng, caption: caption)
         pickMemos?.append(pickMemo)
         createMarker(latlng: latlng)
+        
+        
+        isRegisterCaption = true
         
         /*
          해당 지점에 마커가 있다? > 해당 내용 보여주는 VC
@@ -107,32 +141,4 @@ extension ViewController: NMFMapViewTouchDelegate {
     // TODO: 마커가 찍히는 부분을 캡션값이 있는 것에 한에서 찍어줄 것. 캡션을 벗어나는 값이면 찍지 않기
     // 현재 문제점은 캡션을 벗어난 곳을 찍었지만 계속해서 didTapMap 메서드가 불리고 있는점
     // for in 문을 통해 캡션 여부 파악을 진행하지만 정상 동작하지 않는 점
-    
-    
-    func mapView(_ mapView: NMFMapView, didTap symbol: NMFSymbol) -> Bool {
-        caption = symbol.caption
-        
-        guard let pickMemos = pickMemos else { return true }
-//        for pickmemo in pickMemos {
-//            if pickmemo.caption == caption {
-//                print("이미 존재하는 값")
-//                return true
-//            } else {
-//                print("새로운 값")
-//                return false
-//            }
-//        }
-        for (index, pickmemo) in pickMemos.enumerated() {
-            print("\(index): \(pickmemo.caption)")
-            if pickmemo.caption == caption {
-                print("이미 존재하는 값")
-                return true
-            } else {
-                print("새로운 값")
-            }
-        }
-        
-        print("새로운 값")
-        return false
-    }
 }
